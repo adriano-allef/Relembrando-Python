23a6ff5a9e5b0582e53f16b95a7cf8955de3adf0
Adriano Allef Pereira <adrianopereira951@outlook.com>
2026-02-11 11:08:37 -0300

    Integração com o banco de dados

---
+++ Banco_de_dados/bancodedados/conexao.py
@@ -0,0 +1,13 @@
+import psycopg
+
+conexao = psycopg.connect('postgresql://postgres:postgres@localhost/biblioteca')
+
+cursor = conexao.cursor()
+
+todos_livros = cursor.execute('select * from livros')
+
+print(todos_livros.fetchall())
+
+todos_autores = cursor.execute('select * from autores')
+
+print(todos_autores.fetchall())
